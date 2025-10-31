b7e7b4c24e3851f3021ef329b07ba4e830253d54
Agalakumbura Mudiyanselage Shamal Sharanga <sharangashamal4@gmail.com>
2025-10-31 13:58:13 +0530

    Restoring model Borrow.java to the right place

--- SmartLibrary/src/main/java/com/example/SmartLibrary/model/Borrow.java
+++ SmartLibrary/src/main/java/com/example/SmartLibrary/model/Borrow.java
@@ -1,9 +1,8 @@
-git statuspackage com.example.SmartLibrary.model;
+package com.example.smartlibrary.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
 import java.time.LocalDate;
 
 @Getter
